c47d3612bd5ac5f75489576ff449c5715333764d
tnl001 <62983065+tnl001@users.noreply.github.com>
2021-02-20 23:31:52 -0800

    Added changes to Customer Home, Add, WaitlistTable

--- MRGA/App.js
+++ MRGA/App.js
@@ -1,29 +1,38 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { View } from 'react-native';
+import { Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 
 import CustomerHome from './app/CustomerHome';
 import CustomerAdd from './app/CustomerAdd';
 
-const Stack = createStackNavigator();
+
+// const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-          <Stack.Screen 
-              name="HOME" 
-              component={CustomerHome}
-          />
+    <BrowserRouter>
+      <Route exact path="/"><CustomerHome /></Route>
+      <Route path="/CustomerAdd"><CustomerAdd /></Route>
+    </BrowserRouter>
 
-          <Stack.Screen 
-              name="ADD TO WAITLIST"
-              component={CustomerAdd}
-          />
-      </Stack.Navigator>
-  </NavigationContainer>
+  //   <NavigationContainer>
+  //     <Stack.Navigator>
+  //         <Stack.Screen 
+  //             name="HOME" 
+  //             component={CustomerHome}
+  //         />
+
+  //         <Stack.Screen 
+  //             name="ADD TO WAITLIST"
+  //             component={CustomerAdd}
+  //         />
+  //     </Stack.Navigator>
+  // </NavigationContainer>
   );
 }
 
